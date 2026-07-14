6551c4dd77c66dd647c68d2544743ec7fcc3f889
Kridiyatravels <joshuak24122008@gmail.com>
2026-07-14 14:40:30 +0400

    delete all file

--- enquiry-config.js
+++
@@ -1,5 +0,0 @@
-window.KRIDIYA_ENQUIRY_CONFIG = {
-  formSubmitEndpoint: "https://formsubmit.co/ajax/Info@kridiyatravel.com",
-  whatsappNumber: "971509413873",
-  businessEmail: "Info@kridiyatravel.com"
-};
